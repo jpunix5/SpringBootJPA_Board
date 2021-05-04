7849d0053f5ea6a5e5c5ec382eb6cd6b19ad1b40
jpunix5 <Jpunix5@gmail.com>
2021-05-04 17:06:55 +0900

    code base bone upload

---
+++ SpringBootJPA_Board/src/main/java/com/Board/SpringBootJpaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaBoardApplication.class, args);
+	}
+
+}
